98d5eb2a71529942b5f74c9ef8df2c9521f4f919
Emil Sharier <emil.sharier.dev@gmail.com>
2023-08-17 00:05:42 +0530

    Adding docusaurus

--- apps/docs/next.config.mjs
+++
@@ -1,9 +0,0 @@
-await import('./env.mjs')
-
-/** @type {import('next').NextConfig} */
-const config = {
-  reactStrictMode: true,
-  transpilePackages: ["ui", "eslint-config-custom", "@emilshr/notifi", "tsconfig"],
-};
-
-export default config;
