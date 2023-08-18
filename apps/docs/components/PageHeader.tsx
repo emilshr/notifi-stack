44aa13eb9352d51e6843cd4891d4ed9e62ad7e94
Emil Sharier <emil.sharier.dev@gmail.com>
2023-08-18 13:18:11 +0530

    Adding favicons

---
+++ apps/docs/components/PageHeader.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { useConfig } from "nextra-theme-docs";
+
+export default function PageHeader() {
+  const { frontMatter } = useConfig();
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content={frontMatter.title || "Notifi"} />
+      <meta
+        property="og:description"
+        content={frontMatter.description || "The next site builder"}
+      />
+      <Link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,13 +1,32 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import Link from "next/link";
+import PageHeader from "./components/PageHeader";
+import NotifiLogo from "./components/notifi_logo.png";
+import Image from "next/image";
 
 const config: DocsThemeConfig = {
   logo: <span style={{ fontWeight: 800 }}>Notifi</span>,
   project: {
     link: "https://github.com/emilshr/notifi-stack",
+    icon: <Image alt="Notifi" src={NotifiLogo} width={32} height={32} />,
   },
   footer: {
     text: "Notifi documentation",
+  },
+  head: PageHeader,
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - Notifi",
+    };
+  },
+  notFound: {
+    labels: "Uh oh! Not found",
+    content: <></>,
+  },
+
+  sidebar: {
+    toggleButton: true,
   },
 };
 
