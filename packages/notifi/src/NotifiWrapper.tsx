bab99ee0d74548877767a6008b76736cb1e610dc
Emil Sharier <emil.sharier.dev@gmail.com>
2023-08-24 00:22:56 +0530

    Finalising docs v0.1

--- packages/notifi/src/NotifiWrapper.tsx
+++ packages/notifi/src/NotifiWrapper.tsx
@@ -5,15 +5,40 @@ import type { PackageRouter } from "@web/src/server/api/root";
 import { loggerLink, httpBatchLink, createTRPCProxyClient } from "@trpc/client";
 
 export type NotifiWrapperProps = {
+  /**
+   * @type {string}
+   * @description The API key generated from the project. The value can be copied from the API Keys tab on the project dashboard
+   */
   projectApiKey: string;
+  /**
+   * @type {string}
+   * @description The public ID of the project. The value can be copied from the API Keys tab on the project dashboard
+   */
   projectId: string;
-  connectionEndpoint: string;
+  /**
+   * @type {string}
+   * @description The domain where the next app is hosted. For example, if the Notifi dashboard is hosted at `https://www.notifi.site`, then pass in the connection Endpoint as `https://www.notifi.site` itself.
+   * @note The API endpoints for the package to work would be hosted under `https://<whichever domain>/api/packageTrpc/`. However you wouldn't need to pass the `/api/packageTrpc` part of the URL
+   * @optional
+   * @note "https://www.notifi.site" by default
+   */
+  connectionEndpoint?: string;
+  /**
+   * @type {string}
+   * @description A flag to silently print errors on the console
+   * @optional
+   * @note true by default
+   */
   logErrors?: boolean;
 };
 
+/**
+ *
+ * @description A wrapper component for your React component tree that will register couple of listeners on the DOM which would collect and report unhandled errors & runtime exceptions that occurs in your web app.
+ */
 export const NotifiWrapper = ({
   children,
-  connectionEndpoint,
+  connectionEndpoint = "https://www.notifi.site",
   projectApiKey,
   projectId,
   logErrors = true,
@@ -41,35 +66,37 @@ export const NotifiWrapper = ({
   );
 
   const registerErrorListeners = useCallback(() => {
-    window.addEventListener("error", (ev) => {
-      const { message, colno, lineno, error } = ev;
-      if (logErrors) {
-        console.log(
-          "[NOTIFI] Error detected: ",
-          {
-            message,
-            colno,
-            lineno,
-            error,
-          },
-          error.stack
-        );
-      }
-      const parsedError = new Error(error);
+    if (window.location.hostname.toLowerCase() !== "localhost") {
+      window.addEventListener("error", (ev) => {
+        const { message, colno, lineno, error } = ev;
+        if (logErrors) {
+          console.log(
+            "[NOTIFI] Error detected: ",
+            {
+              message,
+              colno,
+              lineno,
+              error,
+            },
+            error.stack
+          );
+        }
+        const parsedError = new Error(error);
 
-      trpcClient.package.writeErrorLog
-        .mutate({
-          error: {
-            message: parsedError.message,
-            stack: parsedError.stack,
-          },
-          colNo: colno,
-          lineNo: lineno,
-        })
-        .then((data) => {
-          console.log({ data });
-        });
-    });
+        trpcClient.package.writeErrorLog
+          .mutate({
+            error: {
+              message: parsedError.message,
+              stack: parsedError.stack,
+            },
+            colNo: colno,
+            lineNo: lineno,
+          })
+          .then((data) => {
+            console.log({ data });
+          });
+      });
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
